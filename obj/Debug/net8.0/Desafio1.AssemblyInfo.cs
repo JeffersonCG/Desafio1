d9f8659fd5f160297f41fcd627af41d0860c4636
JeffersonCG <jeffersonc_ag@hotmail.com>
2025-04-11 12:39:42 -0300

    Assembly

--- obj/Debug/net8.0/Desafio1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Desafio1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+924f6845c22f6c6474357f6f7a681f96abba2ab9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
